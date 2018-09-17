3abda6ca9aeb92484a2113e66d1fd05b3bf8dd76
warriv93 <warriv93@gmail.com>
2018-09-17 13:15:04 +0545

    asf

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function isAuthenticated() {
 
 
 window.addEventListener('DOMContentLoaded', function () {
-  console.log("app loaded 24");
+  console.log("app loaded 25");
   
   var homeViewBtn = document.getElementById('btn-home-view');
   var loginBtn = document.getElementById('btn-login');
