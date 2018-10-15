2cbb2da671398d17fab2c14120ebabda5166a873
warriv93 <warriv93@gmail.com>
2018-10-15 12:52:27 +0545

    welcomepage

--- welcome.js
+++ welcome.js
@@ -1,4 +1,4 @@
- (!isAuthenticated()) window.location.href = '/index.html';
+ if (!isAuthenticated()) window.location.href = '/index.html';
 
 window.addEventListener('DOMContentLoaded', function () {
     var BotUserOAuthAccessToken = null;
